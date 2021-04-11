f6da1f87b9ee5edd85399623f10fdf8641e5ed00
Jadson <jadsombruno2016@gmail.com>
2021-04-11 18:12:14 -0300

    Update post page to allow preview mode, comments and posts transitions

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -11,15 +11,19 @@ import { format } from 'date-fns';
 import { RichText } from 'prismic-dom';
 import ptBR from 'date-fns/locale/pt-BR';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './post.module.scss';
 import Header from '../../components/Header';
+import Comments from '../../components/Comments';
+import common from '../../styles/common.module.scss';
 
 /**
  * TYPES
  */
 interface Post {
   first_publication_date: string | null;
+  lastEdition: string | null;
   data: {
     title: string;
     banner: {
@@ -35,8 +39,21 @@ interface Post {
   };
 }
 
+interface Navigation {
+  prevPost: {
+    uid: string | null;
+    title: string;
+  };
+  nextPost: {
+    uid: string | null;
+    title: string;
+  };
+}
+
 interface PostProps {
   post: Post;
+  navigation: Navigation;
+  preview: boolean;
 }
 
 /**
@@ -49,7 +66,7 @@ export default function Post(props: PostProps): JSX.Element {
     return <div>Carregando...</div>;
   }
 
-  const { post } = props;
+  const { post, navigation, preview } = props;
 
   const words = post.data.content.reduce((acc, item) => {
     const w = RichText.asText(item.body).split(' ').length;
@@ -68,19 +85,31 @@ export default function Post(props: PostProps): JSX.Element {
         <main className={styles.contentContainer}>
           <h1>{post?.data.title}</h1>
           <div>
-            <time className={styles.infoItem}>
-              <FiCalendar />
-              {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
-                locale: ptBR,
-              })}
-            </time>
-            <span className={styles.infoItem}>
-              <FiUser />
-              {post?.data.author}
-            </span>
-            <span className={styles.infoItem} data-testid="time">
-              <FiClock />
-              {Math.ceil(words / 200)} min
+            <div>
+              <time className={styles.infoItem}>
+                <FiCalendar />
+                {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+                  locale: ptBR,
+                })}
+              </time>
+              <span className={styles.infoItem}>
+                <FiUser />
+                {post?.data.author}
+              </span>
+              <span className={styles.infoItem} data-testid="time">
+                <FiClock />
+                {Math.ceil(words / 200)} min
+              </span>
+            </div>
+            <span className={`${styles.infoItem} ${styles.edition}`}>
+              * editado em
+              {format(
+                new Date(post.first_publication_date),
+                ' dd MMM yyyy, hh:mm',
+                {
+                  locale: ptBR,
+                }
+              )}
             </span>
           </div>
           <section>
@@ -95,6 +124,32 @@ export default function Post(props: PostProps): JSX.Element {
               </React.Fragment>
             ))}
           </section>
+          <footer className={styles.footerContainer}>
+            {navigation.prevPost && (
+              <div>
+                <span>{navigation.prevPost.uid}</span>
+                <Link href={`/post/${navigation.prevPost.uid}`}>
+                  <a>Post anterior</a>
+                </Link>
+              </div>
+            )}
+            {navigation.nextPost && (
+              <div className={styles.nextPost}>
+                <span>{navigation.nextPost.title}</span>
+                <Link href={`/post/${navigation.nextPost.uid}`}>
+                  <a>Próximo post</a>
+                </Link>
+              </div>
+            )}
+          </footer>
+          <Comments />
+          {preview && (
+            <Link href="/api/exit-preview">
+              <a className={common.exitPreviewButtonContainer}>
+                Sair do modo preview
+              </a>
+            </Link>
+          )}
         </main>
       </div>
     </>
@@ -111,23 +166,62 @@ export const getStaticPaths: GetStaticPaths = async () => {
     }
   );
 
-  // Get the paths we want to pre-render based on posts
+  // get the paths we want to pre-render based on posts
   const paths = posts.results.map(post => ({
     params: { slug: post.uid },
   }));
   return { paths, fallback: true };
 };
 
-export const getStaticProps: GetStaticProps = async context => {
-  const { slug } = context.params;
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+  params,
+}) => {
+  const { slug } = params;
 
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('posts', String(slug), {
     lang: 'pt-br',
+    ref: previewData?.ref ?? null,
   });
+
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
+
+  const prevPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date desc]',
+    }
+  );
+
+  const prevUid =
+    prevPost?.results.length > 0
+      ? {
+          uid: prevPost?.results[0].uid,
+          title: prevPost?.results[0].data.title,
+        }
+      : null;
+  const nextUid =
+    nextPost?.results.length > 0
+      ? {
+          uid: nextPost?.results[0].uid,
+          title: nextPost?.results[0].data.title,
+        }
+      : null;
 
   const post: Post = {
     first_publication_date: response.first_publication_date,
+    lastEdition: response.last_publication_date,
     ...response,
     data: {
       ...response.data,
@@ -144,6 +238,14 @@ export const getStaticProps: GetStaticProps = async context => {
   };
 
   return {
-    props: { post },
+    props: {
+      post,
+      navigation: {
+        prevPost: prevUid,
+        nextPost: nextUid,
+      },
+      preview,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
